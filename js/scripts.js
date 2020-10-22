28021336eb0862246d1b4b2f756c7a6da9629eaf
Tawnee Harris <tawneeh@icloud.com>
2020-10-22 15:48:14 -0700

    initial html and js
    Co-authored-by: Erica Woodmansee <ewoodmansee@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form#questions").submit(function() {
+    event.preventDefault();
+    let count = 0;
+    if (question1, question2, question3 === "yes") {
+    count++;
+    }
+      if (count === "3") {
+      alert("CAT!")
+      }
+
+  });
+});
